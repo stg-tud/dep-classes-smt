1b392f353cd217e4964e4537245a36507b472652
gnush <1129861+gnush@users.noreply.github.com>
2018-11-19 15:53:50 +0100

    fixed compiler warnings in TestZ3Solver

--- src/test/scala/smtlib/TestZ3Solver.scala
+++ src/test/scala/smtlib/TestZ3Solver.scala
@@ -19,8 +19,7 @@ class TestZ3Solver extends FunSuite {
   val assert2 = Assert(aaIsC)
   val assert3 = Assert(bbIsC)
   val distinct = Assert(Distinct("a", "b"))
-  val checksat = CheckSat
-  val script = SMTLibScript(Seq(const1, const2, const3, distinct, assert1, assert2, assert3, checksat))
+  val script = SMTLibScript(Seq(const1, const2, const3, distinct, assert1, assert2, assert3, CheckSat))
 
   val z3: Z3Solver = new Z3Solver(SMTLibScript(Seq()))
 
@@ -42,14 +41,14 @@ class TestZ3Solver extends FunSuite {
 
     val assert4 = Assert(Not(Eq("a", 0)))
 
-    z3.addCommands(Seq(assert1, assert2, assert3, assert4, checksat, GetModel))
+    z3.addCommands(Seq(assert1, assert2, assert3, assert4, CheckSat, GetModel))
 
     assert(preSize + 6 == z3.commands.size)
     assert(z3.commands(preSize) == assert1)
     assert(z3.commands(preSize+1) == assert2)
     assert(z3.commands(preSize+2) == assert3)
     assert(z3.commands(preSize+3) == assert4)
-    assert(z3.commands(preSize+4) == checksat)
+    assert(z3.commands(preSize+4) == CheckSat)
     assert(z3.commands(preSize+5) == GetModel)
   }
 
@@ -58,11 +57,11 @@ class TestZ3Solver extends FunSuite {
 
     assert(status == 0)
     assert(output.size > 1)
-    assert(output(0) == "sat")
+    assert(output.head == "sat")
   }
 
   test("Flush") {
-    z3.flush
+    z3.flush()
     assert(z3.commands.isEmpty)
   }
 
@@ -72,6 +71,6 @@ class TestZ3Solver extends FunSuite {
 
     assert(status == 0)
     assert(output.size == 1)
-    assert(output(0) == "unsat")
+    assert(output.head == "unsat")
   }
 }
