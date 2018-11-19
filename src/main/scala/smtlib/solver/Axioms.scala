8b0c19653affaa22d5c70e076342398aed71a386
gnush <1129861+gnush@users.noreply.github.com>
2018-11-19 14:44:45 +0100

    axioms: constraint datatype

---
+++ src/main/scala/smtlib/solver/Axioms.scala
@@ -0,0 +1,55 @@
+package smtlib.solver
+
+import smtlib.SMTLibScript
+import smtlib.syntax._
+import smtlib.syntax.Implicit._
+
+object Axioms {
+  /**
+    * Path datatype declaration
+    * Constrcutors:
+    *   var String
+    *   pth Path String
+    */
+  val pathDatatype = DeclareDatatype("Path", ConstructorDatatype(Seq(
+    ConstructorDec("var", Seq(SelectorDec("id", "String"))),
+    ConstructorDec("pth", Seq(
+      SelectorDec("obj", "Path"),
+      SelectorDec("field", "String")
+    ))
+  )))
+
+  /**
+    * Constraint datatype declaration
+    * Constructors:
+    *   path-eq Path Path
+    *   instance-of Path String
+    *   instantiated-by Path String
+    */
+  val constraintDatatype = DeclareDatatype(SimpleSymbol("Constraint"), ConstructorDatatype(Seq(
+    ConstructorDec(SimpleSymbol("path-eq"), Seq(
+      SelectorDec(SimpleSymbol("p-left"), SimpleSymbol("Path")),
+      SelectorDec(SimpleSymbol("p-right"), SimpleSymbol("Path")))),
+    ConstructorDec(SimpleSymbol("instance-of"), Seq(
+      SelectorDec(SimpleSymbol("instance"), SimpleSymbol("Path")),
+      SelectorDec(SimpleSymbol("cls"), SimpleSymbol("String")))),
+    ConstructorDec(SimpleSymbol("instantiated-by"), Seq(
+      SelectorDec(SimpleSymbol("obj"), SimpleSymbol("Path")), // TODO: find better selecor names for this
+      SelectorDec(SimpleSymbol("inst"), SimpleSymbol("String"))))
+  )))
+
+  /** String is a class name  */
+  val classProp = DeclareFun("class", Seq("String"), Bool)
+
+  /** String is a variable name  */
+  val varProp = DeclareFun("variable", Seq("String"), Bool)
+
+//  val isPathEqProp = DeclareFun(SimpleSymbol("isPathEq"), Seq(SimpleSymbol("Constraint")), Bool)
+////  val isPathEqProp = DefineFun(FunctionDef(SimpleSymbol("isPathEq"), Seq(SortedVar(SimpleSymbol("c"), SimpleSymbol("Constraint"))), Bool,
+////    Exists(Seq(SortedVar(SimpleSymbol("p1"), SimpleSymbol("Path")), SortedVar(SimpleSymbol("p2"), SimpleSymbol("Path"))),
+////      Eq("c", Apply(SimpleSymbol("pathEq"), Seq(SimpleSymbol("p1"), SimpleSymbol("p2")))))))
+//  val isInstanceOfProp = DeclareFun(SimpleSymbol("isInstanceOf"), Seq(SimpleSymbol("Constraint")), Bool)
+//  val isInstantiatedByProp = DeclareFun(SimpleSymbol("isInstantiatedBy"), Seq(SimpleSymbol("Constraint")), Bool)
+
+  def all: SMTLibScript = SMTLibScript(Seq(pathDatatype, constraintDatatype, classProp, varProp))
+}
