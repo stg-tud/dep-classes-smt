fde8f996723db772ed8a1cbea8e210b4ab7dfc2a
gnush <1129861+gnush@users.noreply.github.com>
2018-11-21 16:46:49 +0100

    added entailment for a_bar entails b_bar, substitution for constraint lists, and C-Prog

--- src/main/scala/smtlib/solver/Axioms.scala
+++ src/main/scala/smtlib/solver/Axioms.scala
@@ -83,6 +83,18 @@ object Axioms {
                 ))
               ))
 
+  // TODO: asdf
+//  val bigAnd = DefineFunRec(
+//                FunctionDef("big-and",
+//                  Seq(
+//                    SortedVar("l", Constraints)
+//                  ),
+//                  Bool,
+//                  Match("l", Seq(
+//                    MatchCase(Pattern("nil"), True()),
+//                    MatchCase(Pattern("insert", Seq("hd", "tl")), And("hd", Apply("big-and", Seq("tl"))))
+//                  ))))
+
   /** String is a class name  */
   val classProp = DeclareFun("class", Seq("String"), Bool)
 
@@ -92,25 +104,25 @@ object Axioms {
   /**
     * Substitution function for paths.
     * param p1 Base path for the substitution
-    * param id Variable name to be substituted
+    * param x Variable name to be substituted
     * param p2 Path to be substituted
-    * return `id` substituted with `p2` in `p1`
+    * return `x` substituted with `p2` in `p1`
     */
   val substPath = DefineFunRec(
                     FunctionDef(
                       "subst-path",
                       Seq(
                         SortedVar("p1", "Path"),
-                        SortedVar("id", "String"),
+                        SortedVar("x", "String"),
                         SortedVar("p2", "Path")
                       ),
                       "Path",
                       Match("p1",
                         Seq(
-                          MatchCase(Pattern("var", Seq("x")),
-                            Ite(Eq("id", "x"), "p2", "p1")),
+                          MatchCase(Pattern("var", Seq("y")),
+                            Ite(Eq("x", "y"), "p2", "p1")),
                           MatchCase(Pattern("pth", Seq("p", "f")),
-                            Apply("pth", Seq(Apply("subst-path", Seq("p", "id", "p2")), "f"))
+                            Apply("pth", Seq(Apply("subst-path", Seq("p", "x", "p2")), "f"))
                           )
                         ))
                     ))
@@ -118,16 +130,16 @@ object Axioms {
   /**
     * Substitution function for constraints.
     * param c Base constraint for the substitution
-    * param id Variable name to be substituted
+    * param x Variable name to be substituted
     * param p Path to be substituted
-    * return `id` substituted with `p` in `c`
+    * return `x` substituted with `p` in `c`
     */
   val substConstraint = DefineFun(
                           FunctionDef(
                             "subst-constraint",
                             Seq(
                               SortedVar("c", "Constraint"),
-                              SortedVar("id", "String"),
+                              SortedVar("x", "String"),
                               SortedVar("p", "Path")
                             ),
                             "Constraint",
@@ -135,17 +147,38 @@ object Axioms {
                               Seq(
                                 MatchCase(Pattern("path-eq", Seq("p1", "p2")),
                                   Apply("path-eq", Seq(
-                                    Apply("subst-path", Seq("p1", "id", "p")),
-                                    Apply("subst-path", Seq("p2", "id", "p"))))),
+                                    Apply("subst-path", Seq("p1", "x", "p")),
+                                    Apply("subst-path", Seq("p2", "x", "p"))))),
                                 MatchCase(Pattern("instance-of", Seq("p1", "cls1")),
                                   Apply("instance-of", Seq(
-                                    Apply("subst-path", Seq("p1", "id", "p")),
+                                    Apply("subst-path", Seq("p1", "x", "p")),
                                     "cls1"
                                   ))),
                                 MatchCase(Pattern("instantiated-by", Seq("p1", "cls1")),
                                   Apply("instantiated-by", Seq(
-                                    Apply("subst-path", Seq("p1", "id", "p")),
+                                    Apply("subst-path", Seq("p1", "x", "p")),
                                     "cls1"
+                                  )))
+                              ))
+                          ))
+
+  val substConstraints = DefineFunRec(
+                          FunctionDef(
+                            "subst-constraints",
+                            Seq(
+                              SortedVar("cs", Constraints),
+                              SortedVar("x", "String"),
+                              SortedVar("p", "Path")
+                            ),
+                            Constraints,
+                            Match("cs",
+                              Seq(
+                                MatchCase(Pattern("nil"),
+                                  "nil"),
+                                MatchCase(Pattern("insert", Seq("hd", "tl")),
+                                  Apply("insert", Seq(
+                                    Apply("subst-constraint", Seq("hd", "x", "p")),
+                                    Apply("subst-constraints", Seq("tl", "x", "p"))
                                   )))
                               ))
                           ))
@@ -182,6 +215,7 @@ object Axioms {
     * entails :: List Constraint -> Constraint -> Bool
     */
   val entails = DeclareFun("entails", Seq(Constraints, "Constraint"), Bool)
+  val Entails = DeclareFun("Entails", Seq(Constraints, Constraints), Bool) // TODO: define body
 
   // C-Ident
   private val identTerm = Forall(Seq(SortedVar("c", "Constraint")),
@@ -273,6 +307,30 @@ object Axioms {
                     ))
   val cSubst = Assert(substTerm)
 
+  // C-Prog
+  val progTerm = Forall(
+                  Seq(
+                    SortedVar("cs1", Constraints),
+                    SortedVar("cs2", Constraints),
+                    SortedVar("c", "Constraint"),
+                    SortedVar("x", "String"),
+                    SortedVar("p", "Path"),
+                  ),
+                  Implies(
+                    And(
+                      True(), // TODO: x. cs1 => c in prog
+                      Apply("Entails", Seq(
+                        "cs2",
+                        Apply("subst-constraints", Seq("cs1", "x", "p"))
+                      ))
+                    ),
+                    Apply("entails", Seq(
+                      "cs2",
+                      Apply("subst-constraint", Seq("c", "x", "p"))
+                    ))
+                  ))
+  val cProg = Assert(progTerm)
+
   val entailsOrdering = Assert(
                           Forall(
                             Seq(
@@ -304,8 +362,8 @@ object Axioms {
   val datatypes = Seq(pathDatatype, constraintDatatype)
   val funs = Seq(concat, elem)
   val baseProps = Seq(classProp, varProp)
-  val subst = Seq(substPath, substConstraint, substProp)
-  val sequentCalculus = Seq(entails, cIdent, cRefl, cClass, cCut, cSubst)
+  val subst = Seq(substPath, substConstraint, substConstraints, substProp)
+  val sequentCalculus = Seq(entails, Entails, cIdent, cRefl, cClass, cCut, cSubst, cProg)
   val additionalProperties = Seq(entailsOrdering)
 
   def all: SMTLibScript = SMTLibScript(datatypes ++ funs ++ baseProps ++ subst ++ sequentCalculus ++ additionalProperties)
@@ -313,6 +371,10 @@ object Axioms {
 
 object AxiomsTest extends App {
   val solver = new Z3Solver(Axioms.all, true)
+
+  solver.addCommand(DeclareConst("l", Sorts("List", Seq("Constraint"))))
+  solver.addCommand(DeclareConst("c", "Constraint"))
+  solver.addCommand(Assert(Eq("l", Apply("insert", Seq("c", "nil")))))
 
   solver.addCommand(CheckSat)
 
