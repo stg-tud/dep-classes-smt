8a421e7d4105ced0d5f89276cf5dcf65469a5850
gnush <1129861+gnush@users.noreply.github.com>
2019-08-07 18:05:09 +0200

    added typecheck test of natural numbers prog

---
+++ src/test/scala/dcc/TestTypechecker.scala
@@ -0,0 +1,25 @@
+package dcc
+
+import dcc.syntax._
+import dcc.syntax.Program.Program
+import org.scalatest.FunSuite
+
+class TestTypechecker extends FunSuite {
+    val naturalNumbers: Program = List(
+    ConstructorDeclaration(Id('Zero), Id('x), Nil),
+    ConstraintEntailment(Id('x), List(InstanceOf(Id('x), Id('Zero))), InstanceOf(Id('x), Id('Nat))),
+    ConstructorDeclaration(Id('Succ), Id('x), List(InstanceOf(FieldPath(Id('x), Id('p)), Id('Nat)))),
+    ConstraintEntailment(Id('x), List(InstanceOf(Id('x), Id('Succ)), InstanceOf(FieldPath(Id('x), Id('p)), Id('Nat))), InstanceOf(Id('x), Id('Nat))),
+    AbstractMethodDeclaration(Id('prev), Id('x), List(InstanceOf(Id('x), Id('Nat))), Type(Id('y), List(InstanceOf(Id('y), Id('Nat))))),
+    MethodImplementation(Id('prev), Id('x), List(InstanceOf(Id('x), Id('Zero))), Type(Id('y), List(InstanceOf(Id('y), Id('Nat)))),
+      ObjectConstruction(Id('Zero), Nil)),
+    MethodImplementation(Id('prev), Id('x), List(InstanceOf(Id('x), Id('Succ)), InstanceOf(FieldPath(Id('x), Id('p)), Id('Nat))), Type(Id('y), List(InstanceOf(Id('y), Id('Nat)))),
+      FieldAccess(Id('x), Id('p)))
+  )
+
+  test ("wf natural numbers") {
+    val dcc = new DCC(naturalNumbers)
+
+    assert(dcc.typecheck())
+  }
+}
