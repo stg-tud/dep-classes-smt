279e8c7eb685551de12ccf9ecd9a74652330d4d6
gnush <1129861+gnush@users.noreply.github.com>
2021-04-09 14:04:04 +0200

    added natural numbers program

--- paper/dep-classes/implementation/src/main/scala/Foo.scala
+++ paper/dep-classes/implementation/src/main/scala/Foo.scala
@@ -1,6 +1,7 @@
 import dcc.entailment.SemanticEntailment
+import dcc.program.NaturalNumbers
 import dcc.syntax._
-import dcc.types.Type
+import dcc.types.{IntegratedSubsumptionChecker, Type}
 import smt.smtlib.syntax._
 
 object Foo extends App {
@@ -16,4 +17,12 @@ object Foo extends App {
 
   val sem2 = new SemanticEntailment(Nil)
   sem2.entails(Nil, PathEquivalence(Id(Symbol("y")), Id(Symbol("y"))))
+
+  println("------------------------------------------------")
+
+  // TODO: is there a way to force the program if the entailment to be the same as the program of the checker/interpreter?
+  val sem3 = new SemanticEntailment(NaturalNumbers.program)
+  val checker = new IntegratedSubsumptionChecker(NaturalNumbers.program, sem3)
+
+  println(checker.typecheck) // TODO returns false, one check is sat. investigate
 }
---
+++ paper/dep-classes/implementation/src/main/scala/dcc/program/NaturalNumbers.scala
@@ -0,0 +1,21 @@
+package dcc.program
+
+import dcc.syntax.{AbstractMethodDeclaration, ConstraintEntailment, ConstructorDeclaration, FieldAccess, FieldPath, Id, InstanceOf, MethodImplementation}
+import dcc.syntax.Program.Program
+import dcc.types.Type
+import dcc.syntax.Util.StringToId
+
+// TODO: add trait program
+object NaturalNumbers {
+  val program: Program = List(
+    ConstructorDeclaration("Zero", "x", Nil),
+    ConstraintEntailment("x", List(InstanceOf("x", "Zero")), InstanceOf("x", "Nat")),
+    ConstructorDeclaration("Succ", "x", List(InstanceOf(FieldPath("x", "p"), "Nat"))),
+    ConstraintEntailment("x", List(InstanceOf("x", "Succ"), InstanceOf(FieldPath("x", "p"), "Nat")), InstanceOf("x", "Nat")),
+    AbstractMethodDeclaration("prev", "x", List(InstanceOf("x", "Nat")), Type("y", List(InstanceOf("y", "Nat")))),
+    MethodImplementation("prev", "x", List(InstanceOf("x", "Zero")), Type("y", List(InstanceOf("y", "Nat"))),
+      "x"),
+    MethodImplementation("prev", "x", List(InstanceOf("x", "Succ"), InstanceOf(FieldPath("x", "p"), "Nat")), Type("y", List(InstanceOf("y", "Nat"))),
+      FieldAccess("x", "p"))
+  )
+}
