180e9cd4949efdeef8ee968e24ba6ff473c17177
gnush <1129861+gnush@users.noreply.github.com>
2021-08-20 13:44:30 +0200

    added depth property to dcc path

--- paper/dep-classes/implementation/src/main/scala/dcc/syntax/Id.scala
+++ paper/dep-classes/implementation/src/main/scala/dcc/syntax/Id.scala
@@ -4,6 +4,7 @@ case class Id(name: Symbol) extends Path with Expression {
   override def toString: String = name.name
   override def baseName: String = toString
   override def fieldNames: List[String] = Nil
+  override def depth: Int = 0
 
   def +(right: Id): Id = Id(Symbol(name.name + right.name.name))
 }
--- paper/dep-classes/implementation/src/main/scala/dcc/syntax/Path.scala
+++ paper/dep-classes/implementation/src/main/scala/dcc/syntax/Path.scala
@@ -3,12 +3,14 @@ package dcc.syntax
 trait Path {
   def baseName: String
   def fieldNames: List[String]
+  def depth: Int
 }
 
 case class FieldPath(p: Path, f: Id) extends Path {
   override def toString: String = s"$p.$f"
   override def baseName: String = p.baseName
   override def fieldNames: List[String] = f.toString :: p.fieldNames
+  override def depth: Int = 1+p.depth
 }
 
 // also: VariableName extends Path
